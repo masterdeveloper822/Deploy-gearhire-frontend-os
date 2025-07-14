68d574210785fd9a23fe0a6ade54df259ef60abe
masterdeveloper822 <breeyancornelius051989@gmail.com>
2025-07-14 18:00:58 -0400

    Refactor HeroSection component: Adjust padding and margin classes for improved responsiveness and visual consistency on the onboarding landing page.

--- src/pages/onboarding/landingPage/components/heroSection.tsx
+++ src/pages/onboarding/landingPage/components/heroSection.tsx
@@ -8,7 +8,7 @@ import { Typography } from "@/src/components/ui/typography";
 
 export default function HeroSection() {
   return (
-    <section className="w-full flex justify-center py-8 sm:py-14 md:py-0">
+    <section className="w-full flex justify-center py-8 sm:py-0">
       <div className="w-full max-w-[1350px] flex flex-col sm:flex-row gap-6 sm:gap-12 px-4 md:px-0">
         {/* Left: Text */}
         <div className="w-full pt-0 md:pt-5 lg:pt-14 md:w-[670px] flex flex-col items-center md:items-start justify-center">
@@ -20,7 +20,7 @@ export default function HeroSection() {
           md:mb-10">
             The easiest way to post RFQs, browse gear, and connect with trusted merchants in the film production industry.
           </p>
-          <div className="flex gap-4 w-full">
+          <div className="flex gap-4 w-full sm:pr-20 xl:pr-60">
             <a
               href="/renter-signup"
               className="bg-sky-600 text-white rounded-lg h-[60px] w-full flex items-center justify-center text-center text-[16px] font-medium transition hover:bg-sky-700"
